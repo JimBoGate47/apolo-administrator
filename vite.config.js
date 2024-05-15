1a7195d50c2d83c068b48b8d1a4e63667904ff03
Jimbo <jimbo.gate47@gmail.com>
2024-05-14 20:34:29 -0400

    begining

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  base: "https://JimBoGate47.github.io/apolo-administrator"
+});
